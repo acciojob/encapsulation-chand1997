03aeb2ed3373f3fd0915ed681f522df68a048f0b
chand1997 <chanduchallahalli@gmail.com>
2023-07-11 15:01:36 +0530

    tried setting value for private variable and got error( name has private access in com.driver.RWOnly)

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj=new RWOnly();
+//        obj.name="chandan";
+//        System.out.println(obj.name);
 
     }
   
